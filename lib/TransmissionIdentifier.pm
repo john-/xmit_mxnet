5a41ec8e1ee021cd5f07ec214dab14ea61dd6474
john- <john-@users.noreply.github.com>
2019-08-21 22:15:43 +0000

    perltidy

--- lib/TransmissionIdentifier.pm
+++ lib/TransmissionIdentifier.pm
@@ -36,13 +36,13 @@ use Data::Dumper;
 use feature 'say';
 
 sub new {
-    my ($class, $args) = @_;
+    my ( $class, $args ) = @_;
 
     #say Dumper($args);
     my $self = {
-	hybridize   => $args->{hybridize}   ? $args->{hybridize}   : 0,
-	load_params => $args->{load_params} ? $args->{load_params} : 0,
-	
+        hybridize   => $args->{hybridize}   ? $args->{hybridize}   : 0,
+        load_params => $args->{load_params} ? $args->{load_params} : 0,
+
     };
 
     #say Dumper($self);
@@ -58,34 +58,37 @@ sub _initialize {
 
     my $net = nn->Sequential();
     $net->name_scope(
-	sub {
-	    $net->add(
-		nn->Conv2D( channels => 6, kernel_size => 5, activation => 'relu' )
-   	    );
-	    $net->add( nn->MaxPool2D( pool_size => 2, strides => 2 ) );
-	    $net->add(
-		nn->Conv2D(
-		    channels    => 16,
-		    kernel_size => 3,
-		    activation  => 'relu'
-	        )
-	    );
-	    $net->add( nn->MaxPool2D( pool_size => 2, strides => 2 ) );
-	    $net->add( nn->Flatten() );
-	    $net->add( nn->Dense( 120, activation => "relu" ) );
-	    $net->add( nn->Dense( 84,  activation => "relu" ) );
-	    $net->add( nn->Dense(2) );
-	}
+        sub {
+            $net->add(
+                nn->Conv2D(
+                    channels    => 6,
+                    kernel_size => 5,
+                    activation  => 'relu'
+                )
+            );
+            $net->add( nn->MaxPool2D( pool_size => 2, strides => 2 ) );
+            $net->add(
+                nn->Conv2D(
+                    channels    => 16,
+                    kernel_size => 3,
+                    activation  => 'relu'
+                )
+            );
+            $net->add( nn->MaxPool2D( pool_size => 2, strides => 2 ) );
+            $net->add( nn->Flatten() );
+            $net->add( nn->Dense( 120, activation => "relu" ) );
+            $net->add( nn->Dense( 84,  activation => "relu" ) );
+            $net->add( nn->Dense(2) );
+        }
     );
 
     $net->hybridize() if $self->{hybridize};
     $net->load_parameters('xmit.params') if $self->{load_params};
 
-
     $self->{net} = $net;
 
     $self->{text_labels} = [ 'data', 'voice' ];
-    $self->{ctx} = $self->{cuda} ? mx->gpu(0) : mx->cpu;
+    $self->{ctx}         = $self->{cuda} ? mx->gpu(0) : mx->cpu;
 
 }
 
@@ -100,12 +103,11 @@ sub transformer {
 
     # put channel first
     $data = $data->transpose( [ 2, 0, 1 ] );  # change to channel, height, width
-    #say Dumper($data->shape);
+                                              #say Dumper($data->shape);
     $data = $data->astype('float32') / 255.0;
 
     return ( $data, $label );
 }
-
 
 sub _data_setup {
     my $self = shift;
@@ -113,54 +115,56 @@ sub _data_setup {
     return if exists $self->{train_data};
 
     $self->{train_dataset} = gluon->data->vision->ImageFolderDataset(
-	root      => './training/train',
-	flag      => 1,
-	transform => \&transformer
+        root      => './training/train',
+        flag      => 1,
+        transform => \&transformer
     );
 
     $self->{train_data} = gluon->data->DataLoader(
-	$self->{train_dataset},
-	batch_size => $self->{batch_size},
-	shuffle    => 1,
-	last_batch => 'discard'
+        $self->{train_dataset},
+        batch_size => $self->{batch_size},
+        shuffle    => 1,
+        last_batch => 'discard'
     );
-
 
     $self->{val_dataset} = gluon->data->vision->ImageFolderDataset(
-	root      => './training/test',
-	flag      => 1,
-	transform => \&transformer
+        root      => './training/test',
+        flag      => 1,
+        transform => \&transformer
     );
     $self->{val_data} = gluon->data->DataLoader(
-	$self->{val_dataset},
-	batch_size => $self->{batch_size},
-	shuffle    => 0
+        $self->{val_dataset},
+        batch_size => $self->{batch_size},
+        shuffle    => 0
     );
 
 }
 
 sub _collect_args {
-    my ($self, $args) = @_;
+    my ( $self, $args ) = @_;
 
-    $self->{batch_size} = exists($args->{batch_size}) ? $args->{batch_size} : 1;
-    $self->{cuda}       = exists($args->{cuda})       ? $args->{cuda}       : 0;
-    $self->{epochs}     = exists($args->{epochs})     ? $args->{epochs}     : 20;
-    $self->{lr}         = exists($args->{lr})         ? $args->{lr}         : 0.001;
-    $self->{momentum}   = exists($args->{momentum})   ? $args->{momentum}   : 0.9;
-    $self->{log_intrvl} = exists($args->{log_intrvl}) ? $args->{log_intrvl} : 100;
+    $self->{batch_size} =
+      exists( $args->{batch_size} ) ? $args->{batch_size} : 1;
+    $self->{cuda}     = exists( $args->{cuda} )     ? $args->{cuda}     : 0;
+    $self->{epochs}   = exists( $args->{epochs} )   ? $args->{epochs}   : 20;
+    $self->{lr}       = exists( $args->{lr} )       ? $args->{lr}       : 0.001;
+    $self->{momentum} = exists( $args->{momentum} ) ? $args->{momentum} : 0.9;
+    $self->{log_intrvl} =
+      exists( $args->{log_intrvl} ) ? $args->{log_intrvl} : 100;
 }
 
 sub write_image {
-    my ($self, $data, $location) = @_;
+    my ( $self, $data, $location ) = @_;
 
     $data = $data->at(0)->aspdl;
 
-    if ($data->getdim(2) == 3) {                 # color:
-        $data = $data->reorder(2, 0, 1)->         #  RGB values first
-                        slice('x', 'x', '-1:0');  #  invert image
-    } else {                                     # grayscale:
-        $data = $data->squeeze->                  #  remove uneeded dimension
-                        slice('x', '-1:0');       #  invert image
+    if ( $data->getdim(2) == 3 ) {    # color:
+        $data = $data->reorder( 2, 0, 1 )->slice( 'x', 'x', '-1:0' )
+          ;                           # RGB first then invert
+    }
+    else {                            # grayscale:
+        $data = $data->squeeze->slice( 'x', '-1:0' ); # remove uneeded dimension
+                                                      # then invert image
     }
 
     my $image = ( $data * 255 )->byte;
@@ -169,20 +173,21 @@ sub write_image {
 }
 
 sub dump_all_images {
-    my ($self, $set_ref) = @_;
+    my ( $self, $set_ref ) = @_;
 
     my @set = @$set_ref;
     mkdir "image_dump";
     for my $i ( 0 .. $#set ) {
-        my $data  = ${ $set[$i] }[0];
+        my $data = ${ $set[$i] }[0];
+
         #my $label = ${ $set[$i] }[1];
 
-        $self->write_image($data, "image_dump/$i.png");
+        $self->write_image( $data, "image_dump/$i.png" );
     }
 }
 
 sub get_mislabeled {
-    my ($self, $loader) = @_;
+    my ( $self, $loader ) = @_;
 
     mkdir "mislabeled";
 
@@ -205,14 +210,17 @@ sub get_mislabeled {
         my $otline;
         $otline .= sprintf( "%12s",  $pred ) . " :: ";
         $otline .= sprintf( "%-10s", $true ) . " ";
+
         #$otline .= ( $pred eq $true ) ? ".." : "XX";
-	if ( $pred eq $true ) {
-	    $otline .= "..";
-	} else {
-	    $otline .= "XX";
-	    my $hashish =  int($data->aspdl->sum);
-            $self->write_image($data, sprintf("mislabeled/%s-%s.png", $hashish, $true));
-	}
+        if ( $pred eq $true ) {
+            $otline .= "..";
+        }
+        else {
+            $otline .= "XX";
+            my $hashish = int( $data->aspdl->sum );
+            $self->write_image( $data,
+                sprintf( "mislabeled/%s-%s.png", $hashish, $true ) );
+        }
 
         print $otline . "\n";
 
@@ -220,22 +228,25 @@ sub get_mislabeled {
 }
 
 sub classify {
-    my ($self, $image) = @_;
+    my ( $self, $image ) = @_;
 
-    $image = mx->image->imread( $image );
+    $image = mx->image->imread($image);
+
     # put channel first
-    $image = $image->transpose( [ 2, 0, 1 ] )->expand_dims( axis=>0 );  # change to batch, channel,
-                                                                        # height, width
+    $image = $image->transpose( [ 2, 0, 1 ] )->expand_dims( axis => 0 )
+      ;    # change to batch, channel,
+           # height, width
     $image = $image->astype('float32') / 255.0;
 
     my $prob = $self->{net}->($image)->softmax;
 
-    for my $idx (@{ $prob->topk(k=>0)->at(0) }) {
-	my $i = $idx->asscalar;
-	printf(
-	    "With prob = %.5f, it contains %s\n",
-	    $prob->at(0)->at($i)->asscalar, $self->{text_labels}->[$i]
-	);
+    for my $idx ( @{ $prob->topk( k => 0 )->at(0) } ) {
+        my $i = $idx->asscalar;
+        printf(
+            "With prob = %.5f, it contains %s\n",
+            $prob->at(0)->at($i)->asscalar,
+            $self->{text_labels}->[$i]
+        );
     }
 }
 
@@ -244,7 +255,7 @@ sub test {
 
     my $ctx = $self->{ctx};
 
-    my $metric = mx->metric->Accuracy();
+    my $metric  = mx->metric->Accuracy();
     my $val_set = $self->{val_data};
     while ( defined( my $d = <$val_set> ) ) {
         my ( $data, $label ) = @$d;
@@ -257,7 +268,7 @@ sub test {
 }
 
 sub train {
-    my ($self, $args) = @_;
+    my ( $self, $args ) = @_;
 
     $self->_collect_args($args);
 
@@ -266,19 +277,21 @@ sub train {
     $self->_data_setup;
 
     # Collect all parameters from net and its children, then initialize them.
-    $self->{net}->initialize( mx->init->Xavier( magnitude => 2.24 ), ctx => $ctx );
+    $self->{net}
+      ->initialize( mx->init->Xavier( magnitude => 2.24 ), ctx => $ctx );
 
     # Trainer is for updating parameters with gradient.
     my $trainer = gluon->Trainer( $self->{net}->collect_params(),
-        'sgd', { learning_rate => $self->{lr}, momentum => $self->{momentum} } );
+        'sgd',
+        { learning_rate => $self->{lr}, momentum => $self->{momentum} } );
     my $metric = mx->metric->Accuracy();
     my $loss   = gluon->loss->SoftmaxCrossEntropyLoss();
 
     for my $epoch ( 0 .. $self->{epochs} - 1 ) {
 
-	## set scalars to hold time and mean loss
-	my $time = time();
-	my $lm;
+        ## set scalars to hold time and mean loss
+        my $time = time();
+        my $lm;
 
         # reset data iterator and metric at begining of epoch.
         $metric->reset();
@@ -294,17 +307,17 @@ sub train {
                 # Start recording computation graph with record() section.
                 # Recorded graphs can then be differentiated with backward.
                 my $output;
-		my $L;
+                my $L;
                 autograd->record(
                     sub {
                         $output = $self->{net}->($data);
-                        $L = $loss->( $output, $label );
+                        $L      = $loss->( $output, $label );
                         $L->backward;
                     }
                 );
 
-		## capture the mean loss
-		$lm = PDL::sclr($L->mean->aspdl );
+                ## capture the mean loss
+                $lm = PDL::sclr( $L->mean->aspdl );
 
                 # take a gradient step with batch_size equal to data.shape[0]
                 $trainer->step( $data->shape->[0] );
@@ -312,12 +325,15 @@ sub train {
                 # update metric at last.
                 $metric->update( [$label], [$output] );
 
-                if ( $self->{log_intrvl} > 0 and $i % $self->{log_intrvl} == 0 and $i > 0 ) {
+                if (    $self->{log_intrvl} > 0
+                    and $i % $self->{log_intrvl} == 0
+                    and $i > 0 )
+                {
                     my ( $name, $acc ) = $metric->get();
                     print "[Epoch $epoch Batch $i] Training: $name=$acc\n";
                 }
             },
-            \@{$self->{train_data}}
+            \@{ $self->{train_data} }
         );
 
         my ( $trn_name, $trn_acc ) = $metric->get();
@@ -326,21 +342,22 @@ sub train {
 
         my ( $val_name, $val_acc ) = $self->test;
 
-	## capture time interval
-	my $nowtime = time();
-	my $interval = $nowtime - $time;
+        ## capture time interval
+        my $nowtime  = time();
+        my $interval = $nowtime - $time;
 
-        say sprintf('Epoch %d: loss %.3f, train acc %.3f, test acc %.3f in %0.1f secs',
-		    $epoch, $lm, $trn_acc, $val_acc, $interval);
+        say sprintf(
+            'Epoch %d: loss %.3f, train acc %.3f, test acc %.3f in %0.1f secs',
+            $epoch, $lm, $trn_acc, $val_acc, $interval );
 
     }
-    $self->get_mislabeled($self->{val_data});
+    $self->get_mislabeled( $self->{val_data} );
     $self->{net}->save_parameters('xmit.params');
 
 }
 
 sub info {
-    my ($self, $args) = @_;
+    my ( $self, $args ) = @_;
 
     #$self->{batch_size} = $args->{batch_size} ? $args->{batch_size} : 1;
     $self->_collect_args($args);
@@ -348,7 +365,7 @@ sub info {
     $self->_data_setup;
 
     say $self->net_astext;
-    
+
     my $sample = $self->{train_data}->[0];
     my $data   = $sample->[0];
     my $label  = $sample->[1];
@@ -373,8 +390,8 @@ sub info {
     #print Dumper($train_data);
     #print $train_data->[0][0]->aspdl;
 
-    $self->dump_all_images($self->{train_data});
+    $self->dump_all_images( $self->{train_data} );
 }
-    
+
 1;
 
